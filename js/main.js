c25094c41eaf44a08b26a97977cf91a7a7cb2415
obora <d-obora@mail.ru>
2017-10-19 18:07:03 +0300

    кнопма меню

--- js/main.js
+++ js/main.js
@@ -131,3 +131,24 @@ function updateGradient() {
     }
 }
 //конец переливающегося фона
+//анимация меню
+(function() {
+
+    "use strict";
+
+    var toggles = document.querySelectorAll(".cmn-toggle-switch");
+
+    for (var i = toggles.length - 1; i >= 0; i--) {
+        var toggle = toggles[i];
+        toggleHandler(toggle);
+    };
+
+    function toggleHandler(toggle) {
+        toggle.addEventListener( "click", function(e) {
+            e.preventDefault();
+            (this.classList.contains("active") === true) ? this.classList.remove("active") : this.classList.add("active");
+        });
+    }
+
+})();
+// конец анимации меню
